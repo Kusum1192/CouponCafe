d12279fad222187950ca27346a15980c3ce09f8d
Anil Atri <anilatri10@gmail.com>
2019-12-26 17:02:57 +0530

    Add structure and toolbarmenu,bottommenu

--- app/src/main/java/com/couponcafe/app/MainActivity.java
+++ app/src/main/java/com/couponcafe/app/MainActivity.java
@@ -2,11 +2,23 @@ package com.couponcafe.app;
 
 import android.os.Bundle;
 
+import com.couponcafe.app.fragments.CategoriesFragment;
+import com.couponcafe.app.fragments.HelpFragment;
+import com.couponcafe.app.fragments.HomeFragment;
+import com.couponcafe.app.fragments.ProfileFragment;
+import com.couponcafe.app.fragments.ShopingAssistanceFragment;
+
+import com.couponcafe.app.utils.BottomNavigationViewHelper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,10 +32,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
+//    private AppBarConfiguration mAppBarConfiguration;
+    private BottomNavigationView mBottomNavigationView;
+    private DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +47,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setupBottomNavigation();
+       // drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        if (savedInstanceState == null) {
+            loadHomeFragment();
+        }
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,31 +66,148 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
+
+        //create default navigation drawer toggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        assert navigationView != null;
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+        // Passing each bottom_menu ID as a set of Ids because each
+        // bottom_menu should be considered as top level destinations.
+//        mAppBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+//                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+//                .setDrawerLayout(drawer)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        // Inflate the bottom_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.title_main_menu, menu);
+        MenuItem action_total_amount = menu.findItem(R.id.action_wallet);
+        return true;
+    }
+
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_home:
+                Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_gallery:
+                Toast.makeText(this, "gallery", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void onBackPressed() {
+        assert drawer != null;
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
+
+    public void setupBottomNavigation() {
+
+        mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        BottomNavigationViewHelper.disableShiftMode(mBottomNavigationView);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.navigation_home: {
+                        loadHomeFragment();
+                        return true;
+                    }
+
+                    case R.id.navigation_categories: {
+                        loadCategoriesFragment();
+                        return true;
+                    }
+
+                    case R.id.navigation_shoping_assistance: {
+                        loadShopingAssistanceFragment();
+                        return true;
+                    }
+
+                    case R.id.navigation_help: {
+                    loadHelpFragment();
+                        return true;
+
+                    }
+                    case R.id.navigation_profile: {
+                        loadProfileFragment();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+
+    public void loadHomeFragment() {
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, fragment);
+        ft.commit();
+    }
+
+    public void loadCategoriesFragment() {
+        CategoriesFragment fragment = new CategoriesFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, fragment);
+        ft.commit();
+    }
+
+    public void loadShopingAssistanceFragment() {
+        ShopingAssistanceFragment fragment = new ShopingAssistanceFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, fragment);
+        ft.commit();
+    }
+    public void loadHelpFragment() {
+        HelpFragment fragment = new HelpFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, fragment);
+        ft.commit();
+    }
+
+    public void loadProfileFragment() {
+        ProfileFragment fragment = new ProfileFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, fragment);
+        ft.commit();
+    }
+
 }
---
+++ app/src/main/java/com/couponcafe/app/fragments/ProfileFragment.java
@@ -0,0 +1,33 @@
+package com.couponcafe.app.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.couponcafe.app.R;
+
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.profile_fragment, container, false);
+    }
+
+
+}
