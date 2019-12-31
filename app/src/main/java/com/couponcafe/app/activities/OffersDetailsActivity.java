e6d3b033607fc50ffb0ce49bc57b43e333be49d6
Anil Atri <anilatri10@gmail.com>
2019-12-31 11:40:58 +0530

    design offerdetails page

--- app/src/main/java/com/couponcafe/app/activities/OffersDetailsActivity.java
+++ app/src/main/java/com/couponcafe/app/activities/OffersDetailsActivity.java
@@ -1,26 +1,25 @@
 package com.couponcafe.app.activities;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import com.couponcafe.app.R;
-import com.couponcafe.app.fragments.AllFragment;
-import com.couponcafe.app.fragments.FoodDiningFragment;
-import com.couponcafe.app.fragments.TravelFragment;
-import com.google.android.material.tabs.TabLayout;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class OffersDetailsActivity extends AppCompatActivity {
 
 
+public class OffersDetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    TextView text_copy_code,scratch_win_dialog,mItemDescription;
+    ImageView desc_arrow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,9 +39,71 @@ public class OffersDetailsActivity extends AppCompatActivity {
     }
 
     private void init() {
+        text_copy_code = findViewById(R.id.tv_code_copy);
+        mItemDescription = findViewById(R.id.mItemDescription);
+        desc_arrow = findViewById(R.id.desc_arrow);
+        scratch_win_dialog = findViewById(R.id.scratch_win_dialog);
+        text_copy_code.setOnClickListener(this);
+        desc_arrow.setOnClickListener(this);
+        scratch_win_dialog.setOnClickListener(this);
 
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.share_main_menu, menu);
 
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_code_copy:
+                Toast.makeText(this, "text copied", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.scratch_win_dialog:
+                getcustomDialog();
+                break;
+
+            case R.id.desc_arrow:
+                if (mItemDescription.getVisibility() == View.GONE) {
+                    // it's collapsed - expand it
+                    mItemDescription.setVisibility(View.VISIBLE);
+                    desc_arrow.setImageResource(R.drawable.ic_arrow_up);
+                } else {
+                    // it's expanded - collapse it
+                    mItemDescription.setVisibility(View.GONE);
+                    desc_arrow.setImageResource(R.drawable.ic_arrow_down);
+                }
+
+                break;
+
+
+        }
+    }
+
+    private void getcustomDialog() {
+        final Dialog dialog = new Dialog(OffersDetailsActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.custom_dialoge_scratch_win);
+        dialog.show();
+    }
 }
